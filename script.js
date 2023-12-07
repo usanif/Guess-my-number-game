90c3508e6b381a2e21ad4fce92bbfc5cafb27fa6
usanif <usanif@gmail.com>
2023-12-07 03:43:35 -0600

    Bug fix and added new feature
    Fixed the remaining counting issue. Added a new message section where the player will be notified if the guessed number is close or far. Players don't have to erase their incorrect guess; the input number will be automatically cleared when an attempt is incorrect.

--- script.js
+++ script.js
@@ -1,53 +1,61 @@
 'use strict';
 
 //Create validate logic
-
 let secretNumber = generateRandomNumber();
-const maxAttempts = 6;
-let attempts = 0;
+const maxAttempts = 3;
+let attempts = 1;
+
 document.querySelector('.score').textContent = '?';
 document.querySelector('.number').textContent = '?';
 
-// Function to generate a random number between 1 and 15
+// // Function to generate a random number between 1 and 15
 
 function generateRandomNumber() {
   return Math.trunc(Math.random() * 14) + 1;
 }
 
-//1. Function to restart game
-//2. secret number will reset
-//3. message will reset
-//4. guess will be hidden
-//5. body colour will be reset
-//6. number coloum will be reset
+// //1. Function to restart game
+// //2. secret number will reset
+// //3. message will reset
+// //4. guess will be hidden
+// //5. body colour will be reset
+// //6. number coloum will be reset
 
 function restartGame() {
   secretNumber = generateRandomNumber();
-  attempts = 0;
+  attempts = 1;
   document.querySelector('.number').textContent = '?'; // Change displayed number to a placeholder
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.gameMsg').textContent = '';
 }
 
 //function to handle each game attempt
 
 document.querySelector('.check').addEventListener('click', function () {
   const playerGuess = Number(document.querySelector('.guess').value);
-  // console.log(playerGuess, typeof playerGuess);
 
+  //console.log(playerGuess, typeof playerGuess);
   //Check the number is correct
-
+  // Check if no number is entered
   if (!playerGuess) {
     document.querySelector('.message').textContent = '⛔️ No number!';
-
-    //1. If the number is not equal to secret number (check if guess is correct)//
-    //2. check the attempts of player which is max 6 attempts
-    //3. Won number will be revealed
-    //4. the colour of background will change to green
-    //5. After win the number reveal coloum will become wide
-  } else if (attempts < maxAttempts) {
+    return playerGuess;
+  }
+  const difference = Math.abs(secretNumber - playerGuess);
+  if (difference === 0) {
+    document.querySelector('.gameMsg').textContent = 'You won it!';
+  } else if (difference <= 7) {
+    document.querySelector('.gameMsg').textContent = 'You are very close!';
+  } else if (difference <= 14) {
+    document.querySelector('.gameMsg').textContent = 'You are getting closer.';
+  } else {
+    document.querySelector('.gameMsg').textContent = 'You are far away.';
+  }
+  // Check if the guess is correct
+  if (attempts < maxAttempts) {
     if (playerGuess === secretNumber) {
       document.querySelector('.number').textContent = secretNumber;
       document.querySelector('.score').textContent = secretNumber;
@@ -55,9 +63,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').style.width = '30rem';
 
-      // 1. Set a timer to restart the game after 5 seconds
-      // 2. Display a message with a countdown timer
-
+      // Set a timer to restart the game after 5 seconds
       let countdown = 5;
       const countdownInterval = setInterval(function () {
         document.querySelector(
@@ -70,19 +76,15 @@ document.querySelector('.check').addEventListener('click', function () {
           restartGame(); // Restart the game
         }
       }, 1000);
-
-      //When guess is wrong it display message minus remaining attempt//
     } else {
       attempts++;
-
       document.querySelector(
         '.message'
       ).textContent = `Wrong guess, now you have ${
-        maxAttempts - attempts
+        maxAttempts - attempts + 1
       } attempts remaining.`;
+      document.querySelector('.guess').value = '';
     }
-
-    //When attempt is over it will reveal the secret number and ask to click restart button//
   } else {
     document.querySelector('body').style.backgroundColor = '#FF0000';
     document.querySelector('.number').style.width = '30rem';
@@ -93,6 +95,5 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 
-// Restart the game button//
-
+// Restart the game button
 document.querySelector('.again').addEventListener('click', restartGame);
